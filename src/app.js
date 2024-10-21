88defcb41dd021de7391e6a2f6d7221d2f4cda39
Jason Clarke <jasondanielclarke@gmail.com>
2024-10-21 09:48:35 +0100

    add user location logic

--- src/app.js
+++ src/app.js
@@ -11,3 +11,23 @@ exampleLocations.forEach((location) => {
     console.log(location)
     const marker = L.marker([location.location.lat, location.location.lng]).addTo(map);
 })
+
+// user location logic
+map.locate({setView: true, maxZoom: 16});
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
